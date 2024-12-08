4bab8364b34d515f6de5751a49e07a2c3afa2014
zelfroster <sumitkumarsoni123@gmail.com>
2024-12-08 14:24:50 +0530

    third stage read-eval-print loop

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,18 +8,17 @@ import (
 )
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		// Uncomment this block to pass the first stage
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	reader, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		log.Fatalln(err)
+		// Wait for user input
+		str, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		retmsg := fmt.Sprintf("%s: command not found\n", str[:len(str)-1])
+		fmt.Fprint(os.Stdout, retmsg)
 	}
-
-	bytes := []byte(reader)
-	str := string(bytes[:len(bytes)-1])
-
-	retmsg := fmt.Sprintf("%s: command not found\n", str)
-	fmt.Fprint(os.Stdout, retmsg)
 }
